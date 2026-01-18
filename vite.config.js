2b9b20a503c203d580f82f67b0197a331dd3093e
Ha-Khu <davidplevka@gmail.com>
2026-01-18 15:29:27 +0100

    Nastavenie Vite pre gh pages

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/Workout-tracker/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
